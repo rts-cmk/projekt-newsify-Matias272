41293b3e77fea979a41bf97039dfe3588256b5bd
Matias Jørgensen <matjorrub@gmail.com>
2025-10-26 13:01:56 +0100

    homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import NavBar from "../components/NavBar";
 
 export default function Home() {
   const [type, setType] = useState(null);
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const apiKey = import.meta.env.VITE_API_KEY;
 
   useEffect(() => {
@@ -14,20 +14,49 @@ export default function Home() {
         `/nyt/svc/search/v2/articlesearch.json?q=${type}&api-key=${apiKey}`
       );
       const result = await response.json();
-      setData(result);
+      setData(result.response.docs);
     }
     fetchData();
   }, [type, apiKey]);
-  
   console.log(data);
+
+  const categories = ["health", "sport", "travel"];
 
   return (
     <>
       <Header isShowing />
-      <ul className="news">
-        <li onClick={() => setType("health")}>Health</li>
-        <li onClick={() => setType("sport")}>Sport</li>
-        <li onClick={() => setType("travel")}>Travel</li>
+      <ul className="news__wrapper">
+        {categories.map((category) => {
+          return (
+            <li key={category} className="news__li">
+              <div className="news__top" onClick={() => setType(category)}>
+                <figure>
+                  <img src="../img/newsify_logo.svg" alt="" />
+                  <figcaption>{category}</figcaption>
+                </figure>
+                <img
+                  className="news__top__arrow"
+                  src="../img/icons/flecha.svg"
+                  alt=""
+                />
+              </div>
+              <ul className="news__bottom">
+                {type === category &&
+                  data.map((article) => (
+                    <li key={article._id}>
+                      <a
+                        href={article.web_url}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {article.headline.main}
+                      </a>
+                    </li>
+                  ))}
+              </ul>
+            </li>
+          );
+        })}
       </ul>
 
       <NavBar />
