3c700971e4516d54bcc5a4046e2a6e6abe08f55c
Matias Jørgensen <matjorrub@gmail.com>
2025-10-27 14:16:46 +0100

    fetching done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,6 @@ export default function Home() {
   const apiKey = import.meta.env.VITE_API_KEY;
 
   useEffect(() => {
-    if (!type) return;
     async function fetchData() {
       const response = await fetch(
         `/nyt/svc/search/v2/articlesearch.json?q=${type}&api-key=${apiKey}`
@@ -18,9 +17,9 @@ export default function Home() {
     }
     fetchData();
   }, [type, apiKey]);
-  console.log(data);
+console.log(data);
 
-  const categories = ["health", "sport", "travel"];
+  const categories = ["europe", "health", "sport", "travel", "business"];
 
   return (
     <>
@@ -29,26 +28,32 @@ export default function Home() {
         {categories.map((category) => {
           return (
             <li key={category} className="news__li">
-              <div className="news__top" onClick={() => setType(category)}>
+              <div
+                className="news__top"
+                onClick={() => setType(type === category ? null : category)}
+              >
                 <figure>
                   <img src="../img/newsify_logo.svg" alt="" />
                   <figcaption>{category}</figcaption>
                 </figure>
                 <img
-                  className="news__top__arrow"
+                  className={`news__arrow ${type === category ? "active" : ""}`}
                   src="../img/icons/flecha.svg"
                   alt=""
                 />
               </div>
-              <ul className="news__bottom">
+              <ul
+                className={`news__bottom ${type === category ? "active" : ""}`}
+              >
                 {type === category &&
-                  data.slice(5).map((article) => (
+                  data.map((article) => (
                     <li className="article__li" key={article._id}>
-                      <figure>
+                      <figure className="article__left">
                         <img src={article.multimedia.thumbnail.url} alt="s" />
                         <figcaption>
                           <h3>{article.headline.main}</h3>
-                          <p></p>
+                          <p>{article.snippet}</p>
+                          <a target="_blank" href={article.web_url}>Read more</a>
                         </figcaption>
                       </figure>
                     </li>
